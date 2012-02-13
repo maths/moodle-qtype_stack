52d6e52143cee2a4d4e34448050f01757284353c
Tim Hunt <T.J.Hunt@open.ac.uk>
2012-02-13 19:38:58 +0000

    Fix incorrect URL in the copyright statement
    Before we copy-and-paste it into any more files ;-)

--- lang/en/qtype_stack.php
+++ lang/en/qtype_stack.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-This file is part of Stack - http://stack.bham.ac.uk//
+This file is part of Stack - http://stack.bham.ac.uk/
 
 Stack is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by

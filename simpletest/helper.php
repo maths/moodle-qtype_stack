e0b32be201f35b37a2bfc1b3d23e679aa789b231
Tim Hunt <T.J.Hunt@open.ac.uk>
2012-02-20 15:20:10 +0000

    Start adding the standard test-n questions to helper.php
    The default test question is now the one from test-1.xml. test-0.xml
    also done, and more to come.

--- simpletest/helper.php
+++ simpletest/helper.php
@@ -34,7 +34,8 @@ defined('MOODLE_INTERNAL') || die();
  */
 class qtype_stack_test_helper extends question_test_helper {
     public function get_test_questions() {
-        return array('test0');
+        return array('test1', 'test0', 'test2', 'test3', 'test4', 'test5', 'test6',
+                'test7', 'test8', 'test9');
     }
 
     /**
@@ -47,8 +48,8 @@ class qtype_stack_test_helper extends question_test_helper {
         test_question_maker::initialise_a_question($q);
 
         $q->name = 'Stack question: test0';
-        $q->questionvariables = 'n=rand(3)+2; m=n+rand(3)+2; ta=n+m';
-        $q->questiontext = 'What is $@n@+@m@$? #ans1#
+        $q->questionvariables = '';
+        $q->questiontext = 'What is $1+1$? #ans1#
                            <IEfeedback>ans1</IEfeedback>
                            <PRTfeedback>firsttree</PRTfeedback>';
         $q->generalfeedback = '';
@@ -60,12 +61,51 @@ class qtype_stack_test_helper extends question_test_helper {
 
         $q->prts = array();
             $sans = new stack_cas_casstring('ans1', 't');
-            $tans = new stack_cas_casstring('ta', 't');
-            $pr = new stack_potentialresponse_node($sans, $tans, 'EqualComAss');
-            $pr->add_branch(0, '=', 0, '', -1, 'Come on, add them together!', 'firsttree-0-0');
-            $pr->add_branch(1, '=', 1, '', 1, 'Yeah!', 'firsttree-0-1');
-            $potentialresponses[] = $pr;
-        $q->prts[1] = new stack_potentialresponse_tree('firsttree', '', false, 1, null, $potentialresponses);
+            $tans = new stack_cas_casstring('2', 't');
+            $node = new stack_potentialresponse_node($sans, $tans, 'EqualComAss');
+            $node->add_branch(0, '=', 0, '', -1, '', 'firsttree-0-0');
+            $node->add_branch(1, '=', 1, '', -1, '', 'firsttree-0-1');
+        $q->prts['firsttree'] = new stack_potentialresponse_tree('firsttree', '', false, 1, null, array($node));
+
+        $q->options = new stack_options();
+
+        return $q;
+    }
+
+    /**
+     * @return qtype_stack_question the question from the test0.xml file.
+     */
+    public static function make_stack_question_test1() {
+        question_bank::load_question_definition_classes('stack');
+        $q = new qtype_stack_question();
+
+        test_question_maker::initialise_a_question($q);
+
+        $q->name = 'Stack question: test0';
+        $q->questionvariables = 'n = rand(5)+3; a = rand(5)+3; v = x; p = (v-a)^n; ta = (v-a)^(n+1)/(n+1)';
+        $q->questiontext = 'Find
+                            \[ \int @p@ d@v@\]
+                            #ans1#
+                            <IEfeedback>ans1</IEfeedback>
+                            <PRTfeedback>PotResTree_1</PRTfeedback>';
+        $q->generalfeedback = 'We can either do this question by inspection (i.e. spot the answer)
+                               or in a more formal manner by using the substitution
+                               \[ u = (@v@-@a@).\]
+                               Then, since $\frac{d}{d@v@}u=1$ we have
+                               \[ \int @p@ d@v@ = \int u^@n@ du = \frac{u^@n+1@}{@n+1@}+c = @ta@+c.\]';
+        $q->qtype = question_bank::get_qtype('stack');
+
+        $q->interactions = array();
+        $q->interactions['ans1'] = stack_interaction_controller::make_element(
+                        'algebraic', 'ans1', 20, null);
+
+        $q->prts = array();
+        $sans = new stack_cas_casstring('ans1', 't');
+        $tans = new stack_cas_casstring('ta', 't');
+        $node = new stack_potentialresponse_node($sans, $tans, 'Int', 'x');
+        $node->add_branch(0, '=', 0, '', -1, '', 'PotResTree_1-0-0');
+        $node->add_branch(1, '=', 1, '', -1, '', 'PotResTree_1-0-1');
+        $q->prts['PotResTree_1'] = new stack_potentialresponse_tree('PotResTree_1', '', true, 1, null, array($node));
 
         $q->options = new stack_options();
 

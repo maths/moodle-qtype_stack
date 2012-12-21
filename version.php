61ee0773bc88ed01309ecc0119709815902d99bb
Chris Sangwin <c.j.sangwin@gmail.com>
2012-12-21 11:56:11 +0000

    Minor fix to the feedback given by the factored form answer test. [Note first Maxima version change]

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012121400;
+$plugin->version   = 2012122100;
 $plugin->requires  = 2012062500;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

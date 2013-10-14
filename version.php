e05ad1e3b0ff8118be2e1644bd5c143c00f5e5e9
Chris Sangwin <C.J.Sangwin@gmail.com>
2013-10-14 22:08:30 +0100

    Minor accessibility improvements to underline all terms generated by Maxima in red, in addition to just using colour.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013091900;
+$plugin->version   = 2013101400;
 $plugin->requires  = 2012120300;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

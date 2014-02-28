56ac7f30da30be79f9ace5cbfdd43d5c80545e29
Chris Sangwin <C.J.Sangwin@gmail.com>
2014-02-28 20:40:27 +0000

    Bug fix and further test cases to ATInt.  Thanks to Matti Pauna and Anna Kairema for reporting this problem.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014021700;
+$plugin->version   = 2014022800;
 $plugin->requires  = 2012120300;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

00bb46e63d0d2e76c5f26e9abf6901f39dbdbc2f
Chris Sangwin <C.J.Sangwin@gmail.com>
2014-11-03 16:22:23 +0000

    Update version number to prompt a replacement cache etc.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014101700;
+$plugin->version   = 2014110300;
 $plugin->requires  = 2013101800;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

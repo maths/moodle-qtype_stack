16c2b6177146439e81b39bff09bc2275be67d9d1
Chris Sangwin <C.J.Sangwin@gmail.com>
2015-06-08 12:13:24 +0100

    Fix to issue #128.

--- tests/castext_test.php
+++ tests/castext_test.php
@@ -509,4 +509,17 @@ class stack_cas_text_test extends qtype_stack_testcase {
         $this->assertEquals($at2->get_display_castext(),
                 '\[\int {f\left(x\right)}{\;\mathrm{d}x}\]');
     }
+
+    public function test_strings_in_castext() {
+        $vars = "st1:[\"\;\sin(x^2)\",\"\;\cos(x^2)\"]\n/* And a comment: with LaTeX \;\sin(x) */\n a:3;";
+        $at1 = new stack_cas_keyval($vars, null, 123, 't', true, 0);
+        $this->assertTrue($at1->get_valid());
+
+        $at2 = new stack_cas_text('\[@a@\]', $at1->get_session(), 0, 't');
+        $this->assertTrue($at2->get_valid());
+        $at2->get_display_castext();
+
+        $this->assertEquals($at2->get_display_castext(),
+                '\[3\]');
+    }
 }

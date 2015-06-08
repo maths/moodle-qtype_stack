40fc541585a61b9d6ab1a9cf28fb883e5ec6840e
Chris Sangwin <C.J.Sangwin@gmail.com>
2015-06-08 21:16:28 +0100

    Minor changes to the connector.

--- stack/cas/connector.class.php
+++ stack/cas/connector.class.php
@@ -107,7 +107,6 @@ abstract class stack_cas_connection_base implements stack_cas_connection {
      */
     public function __construct($settings, stack_debug_log $debuglog) {
         global $CFG;
-
         $path = $CFG->dataroot . '/stack';
 
         $initcommand = 'load("' . $path . '/maximalocal.mac");' . "\n";

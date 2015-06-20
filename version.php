81d59b308f14032cec16efb952e0f9d65b5c196b
Chris Sangwin <C.J.Sangwin@gmail.com>
2015-06-20 12:34:40 +0100

    Empty string should not have quotation marks printed: fix to issue #137.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015060800;
+$plugin->version   = 2015062000;
 $plugin->requires  = 2013101800;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

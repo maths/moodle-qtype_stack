74d7a55c24e4c0734906634277fbba786f75f92f
Chris Sangwin <C.J.Sangwin@gmail.com>
2015-09-11 10:41:04 +0100

    Refactor to remove DIV_OP (it has been a while since support for 5.15 disappeared).  Maxima code tidy.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015090900;
+$plugin->version   = 2015091100;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

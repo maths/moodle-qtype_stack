7c7af2d5f727fdd1076d06121b751e6ee20d2715
Chris Sangwin <C.J.Sangwin@gmail.com>
2015-11-13 18:52:38 +0000

    Fix minor bug.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015103000;
+$plugin->version   = 2015111300;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

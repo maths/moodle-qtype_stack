015f600c729d198f8a3438d669476e47166633a1
Chris Sangwin <C.J.Sangwin@gmail.com>
2016-06-01 20:58:14 +0100

    Update version numbers.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016051202;
+$plugin->version   = 2016060100;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

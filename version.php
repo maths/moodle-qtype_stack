89ab19ee09a107945e7cc508443cf69acfc39680
Chris Sangwin <C.J.Sangwin@gmail.com>
2016-06-07 08:52:39 +0100

    Added support for Travis CI.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016060100;
+$plugin->version   = 2016060700;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

6f3b008d2cbe654725120cf0fc715546e518e8c0
Chris Sangwin <C.J.Sangwin@gmail.com>
2016-09-18 16:36:41 +0100

    Reinstate the missing commit c40707e6673345d4e2bf975f8039d735d99b2850.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016082901;
+$plugin->version   = 2016091700;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

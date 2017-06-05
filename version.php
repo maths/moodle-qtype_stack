a67645fcef85b3430a54f4100266949761c26277
Matti Harjula <matti.harjula@aalto.fi>
2017-06-05 18:35:42 +0300

    fix version number typo.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017060600;
+$plugin->version   = 2017060500;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

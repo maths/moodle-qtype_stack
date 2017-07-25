ff6c5141d1131daf7c8bd0efd820b05b37c5c08d
Maksim Kraev <maxim.kraev@gmail.com>
2017-07-25 15:11:56 +0100

    endpoint modifications

--- api/endpoint.php
+++ api/endpoint.php
@@ -1,64 +1,73 @@
 <?php
+
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 ini_set('html_errors', 1);
 
-require_once(__DIR__ . '/config.php');
+require_once(__DIR__ . '../config.php');
 
-require_once(__DIR__ . '/api/api.php');
-require_once(__DIR__ . '/question.php');
+require_once(__DIR__ . './api.php');
+require_once(__DIR__ . '../question.php');
+
+function printData($data) {
+  header('Content-Type: application/json');
+  echo json_encode($data);
+}
+
+function printError($message) {
+  $res = [
+    "error": true,
+    "message": $message,
+  ];
+  printData($res);
+  exit;
+}
+
+function validateData($data) {
+  // TODO:
+}
+
+function parseInput() {
+  $data = $_POST['data']
+  if ($data === null) {
+    printError('No data sent')
+  }
+  $parsed = json_decode($data, true);
+  validateData($parsed);
+  return $parsed;
+}
 
 $api = new qtype_stack_api();
 
-$request = $_POST
+$parsed = parseInput();
 
-switch($request['type']) {
-  case 'question_html':
-    $response = get_question($request);
-    break;
-  case 'validate':
-    $response = validate($request);
-    break;
-  default:
-    $response = 'Request is invalid';
-}
+$questionxml = $parsed['question'];
 
-function get_question($request) {
-  $questionxml = $request['question'];
-  $question = $api->initialise_question_from_xml($questionxml);
-  $options = $request['options'];
-  question->seed = 10384;
+$question = $api->initialise_question_from_xml($questionxml);
+// Make this a definite number, to fix the random numbers.
+$question->seed = $parsed['seed'];
+//print_r($question);
 
-  $question->initialise_question_from_seed();
+$question->initialise_question_from_seed();
 
-  // Control the display of feedback, and whether students can change their answer.
-  $options = new stdClass();
-  $options->readonly = $options['readonly'];
-  $options->feedback = $options['feedback'];
-  $options->score = $options['score'];
-  // Show a worked solution?
-  $options->generalfeedback = $options['generalfeedback'];
+// Control the display of feedback, and whether students can change their answer.
+$options = new stdClass();
+$options->readonly = $parsed['readOnly'];
+// Do we display feedback and a score for each part (in a multi-part question)?
+$options->feedback = $parsed['feedback'];
+$options->score = $parsed['score'];
 
-  return $api->formulation_and_controls($question, null, $options, '');
-}
+$attempt = $parsed['answer'];
 
-function validate($request) {
-  $questionxml = $request['question'];
-  $question = $api->initialise_question_from_xml($questionxml);
-  $response = $request['response'];
-  $options = $request['options'];
-  question->seed = 10384;
+$result = $api->formulation_and_controls($question, $attempt, $options, '');
 
-  $question->initialise_question_from_seed();
+$json = [
+  "questiontext" => $res->questiontext,
+  "score" => $res->score,
+  "generalfeedback" => $res->generalfeedback,
+  "formatcorrectresponse" => $res->formatcorrectresponse,
+  "summariseresponse" => $res->summariseresponse,
+  "answernotes" => $res->answernotes
+]
 
-  // Control the display of feedback, and whether students can change their answer.
-  $options = new stdClass();
-  $options->readonly = $options['readonly'];
-  $options->feedback = $options['feedback'];
-  $options->score = $options['score'];
-  // Show a worked solution?
-  $options->generalfeedback = $options['generalfeedback'];
-
-  return $api->formulation_and_controls($question, $response, $options, '');
-}
-
+printData($json)

22fc061bbcccd8641ec8d70a22df02a1323557d0
Maksim Kraev <maxim.kraev@gmail.com>
2017-07-26 16:34:07 +0100

    restire original file

--- stack/cas/connectorhelper.class.php
+++ stack/cas/connectorhelper.class.php
@@ -365,6 +365,7 @@ abstract class stack_connection_helper {
         self::ensure_config_loaded();
 
         $imagename = stack_utils::convert_slash_paths($CFG->dataroot . '/stack/maxima_opt_auto');
+
         $lisp = '1';
         // Try to guess the lisp version.
         if (!(false === strpos($genuinedebug, 'GNU Common Lisp (GCL)'))) {

5401237b6e6fef574ac283f4633a5993cd7a0c11
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2018-02-05 16:16:39 +0000

    Remove noun forms from the teacher's answer in algebraic inputs.

--- tests/input_algebraic_test.php
+++ tests/input_algebraic_test.php
@@ -126,6 +126,9 @@ class stack_algebra_input_test extends qtype_stack_testcase {
         $el = stack_input_factory::make('algebraic', 'sans1', 'x^2/(1+x^2)');
         $state = $el->validate_student_response(array('sans1' => 'x^2'), $options, 'x^2/(1+x^2)', null);
         $this->assertEquals(stack_input::VALID, $state->status);
+        $this->assertEquals('A correct answer is <span class="filter_mathjaxloader_equation">'
+          . '<span class="nolink">\( \frac{x^2}{1+x^2} \)</span></span>, which can be typed in as follows: '
+          . '<code>x^2/(1+x^2)</code>', $el->get_teacher_answer_display('x^2/(1+x^2)', '\frac{x^2}{1+x^2}'));
     }
 
     public function test_validate_student_response_2() {
@@ -191,6 +194,19 @@ class stack_algebra_input_test extends qtype_stack_testcase {
         $state = $el->validate_student_response(array('sans1' => 'x^2+0.33'), $options, 'x^2+1/3', array('tans'));
         $this->assertEquals(stack_input::INVALID, $state->status);
         $this->assertEquals('Illegal_floats', $state->note);
+    }
+
+    public function test_validate_student_response_9() {
+        $options = new stack_options();
+        $el = stack_input_factory::make('algebraic', 'sans1', '1<x nounand x<7');
+        $state = $el->validate_student_response(array('sans1' => '1<x and x<7'), $options, '1<x nounand x<7',
+            array('tans'));
+        $this->assertEquals(stack_input::VALID, $state->status);
+        $this->assertEquals('1<x nounand x<7', $state->contentsmodified);
+        $this->assertEquals('\[ 1 < x\,{\mbox{ and }}\, x < 7 \]', $state->contentsdisplayed);
+        $this->assertEquals('A correct answer is <span class="filter_mathjaxloader_equation">'
+            . '<span class="nolink">\( 1<x \,{\mbox{and}}\,x<7 \)</span></span>, which can be typed in as follows: '
+            . '<code>1<x and x<7</code>', $el->get_teacher_answer_display('1<x nounand x<7', '1<x \,{\mbox{and}}\,x<7'));
     }
 
     public function test_validate_student_lowest_terms_1() {

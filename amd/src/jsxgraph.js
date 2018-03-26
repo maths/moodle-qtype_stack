106ff54e2983a93a4646d8e66edde7eb89682797
Matti Harjula <matti.harjula@aalto.fi>
2018-03-26 10:40:57 +0300

    Lazy load the jsxgraph library so that we do not need to load 500+k on each page unless actually needed. Still conflicts with the official filter.

--- amd/src/jsxgraph.js
+++ amd/src/jsxgraph.js
@@ -1,6 +1,6 @@
 
 // So lets hope this is the correct way to name a Moodle AMD module
-define(['qtype_stack/jsxgraphcore', 'core/yui'], function(JXG, Y) {
+define(['qtype_stack/jsxgraphcore-lazy', 'core/yui'], function(JXG, Y) {
     return {
             init: function(divid, code) {
                 // This is bad but as we cannot pass direct code as anything but a string...

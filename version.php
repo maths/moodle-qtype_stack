11fe0cc2026b5c2315ba4522957427c190c7eed2
Chris Sangwin <C.J.Sangwin@gmail.com>
2018-06-03 08:46:55 +0100

    Starting point for the STACK 4.2 release.

--- version.php
+++ version.php
@@ -24,12 +24,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018021901;
+$plugin->version   = 2018060600;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '4.1 for Moodle 2.7+';
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '4.2 for Moodle 2.7+';
 
 $plugin->dependencies = array(
     'qbehaviour_adaptivemultipart'     => 2015090900,

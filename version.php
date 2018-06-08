fed70e58491e407baabd485cf32b82e006fc3297
Matti Harjula <matti.harjula@aalto.fi>
2018-06-08 12:21:55 +0300

    Add the load statement for JSON parsing functions to the maxima code and provide an example of it in action.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018060400;
+$plugin->version   = 2018060800;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

1130d860ebb8e03d78c6c7973ba48c2dfa844685
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2018-07-30 15:24:56 +0100

    Update version number for the 4.2 release.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018072700;
+$plugin->version   = 2018073000;
 $plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

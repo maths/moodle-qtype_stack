23d8936ba5e18a17610dd9eb50325264ffb7ce67
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2018-12-04 13:03:57 +0000

    Ensure the units input type does not display a multiplication sign, even when options are set elsewhere in a question.

--- stack/cas/casstring.class.php
+++ stack/cas/casstring.class.php
@@ -1609,11 +1609,11 @@ class stack_cas_casstring {
         }
         if ($validationmethod == 'units') {
             // Note, we don't pass in forbidfloats as this option is ignored by the units validation.
-            $this->casstring = 'stack_validate_units(['.$starredanswer.'], '.$lowestterms.', '.$tans.', "inline", '.$fltfmt.')';
+            $this->casstring = '(make_multsgn("blank"),stack_validate_units(['.$starredanswer.'], '.$lowestterms.', '.$tans.', "inline", '.$fltfmt.'))';
         }
         if ($validationmethod == 'unitsnegpow') {
             // Note, we don't pass in forbidfloats as this option is ignored by the units validation.
-            $this->casstring = 'stack_validate_units(['.$starredanswer.'], '.$lowestterms.', '.$tans.', "negpow", '.$fltfmt.')';
+            $this->casstring = '(make_multsgn("blank"),stack_validate_units(['.$starredanswer.'], '.$lowestterms.', '.$tans.', "negpow", '.$fltfmt.'))';
         }
 
         return true;

7924e95e24e3c2bea8eeceb3c9630727bb0db770
Vesa Lappalainen <vesal@jyu.fi>
2018-12-09 19:52:33 +0200

    docker server name changed

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-docker rm -f stack-test-container
+docker rm -f stack-api-server
 git pull
 sudo docker build -t stack .
-sudo docker run --rm=true -t -i --security-opt seccomp=unconfined -v plots:/var/data/api/stack/plots --name stack-test-container -p 90:80 stack
+sudo docker run --rm=true -t -i --security-opt seccomp=unconfined -v plots:/var/data/api/stack/plots --name stack-api-server -p 90:80 stack

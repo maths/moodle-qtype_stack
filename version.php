5cc9f44c2ea75872e28dccafa420c3dff08c0f21
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2019-05-17 11:58:05 +0100

    Fix to issue #478.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018121000;
+$plugin->version   = 2019051400;
 $plugin->requires  = 2015111600;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

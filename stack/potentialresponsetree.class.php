8056e8cae35e53ccda0a2551c632e46a683e7c92
Matti Harjula <matti.harjula@aalto.fi>
2019-09-25 14:39:50 +0300

    Issues with the key being lost in certain cases. Better to make it crystal clear.

--- stack/potentialresponsetree.class.php
+++ stack/potentialresponsetree.class.php
@@ -127,10 +127,12 @@ class stack_potentialresponse_tree {
             // Validating as teacher at this stage removes the problem of "allowWords" which
             // we don't have access to.  This effectively allows any words here.  But the
             // student's answer has already been through validation.
-            $cs = stack_ast_container::make_from_teacher_source($name . ':' . $ans, '',
+            $cs = stack_ast_container::make_from_teacher_source($ans, '',
                     new stack_cas_security(), array());
             // That all said, we then need to manually add in nouns to ensure these are protected.
             $cs->set_nounify(true);
+            $cs->set_key($name);
+            $cs->set_keyless(false);
             $cascontext->add_statement($cs);
         }
 

7de912ddd99d8646ef8fa1dc36bfb8e03404ddf1
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2019-10-30 15:21:21 +0000

    Bump version number to match master (to circumvent downgrade error in Moodle).

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019083000;
+$plugin->version   = 2019090200;
 $plugin->requires  = 2015111600;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

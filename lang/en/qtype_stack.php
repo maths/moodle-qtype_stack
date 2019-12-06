45a126112e26208c481ce21d618011c069d0561c
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2019-12-06 13:02:14 +0000

    Fix to issue #521.

--- lang/en/qtype_stack.php
+++ lang/en/qtype_stack.php
@@ -916,6 +916,7 @@ $string['stackDoc_docs_desc']           = '<a href="{$a->link}">Documentation fo
 $string['stackDoc_home']                = 'Documentation home';
 $string['stackDoc_index']               = 'Category index';
 $string['stackDoc_siteMap']             = 'Site map';
+$string['stackDoc_siteMap_en']          = 'English site map';
 $string['stackDoc_404message']          = 'File not found.';
 $string['stackDoc_directoryStructure']  = 'Directory structure';
 $string['stackDoc_version']             = 'Your site is running STACK version {$a}.';

fba84876d8d28b2b617db68890c18dc2498ba25f
Matti Harjula <matti.harjula@aalto.fi>
2019-12-12 10:49:02 +0200

    Improve the singleton number filter to deal with the issues of overly large or small floats 1e310, 1e-310 by turning them to exact values automatically and blocking conversion to float form if the value is that large. If we wish to support floats of such scale elsewhere a similar checks might be required.

--- stack/cas/parsingrules/801_singleton_numeric.filter.php
+++ stack/cas/parsingrules/801_singleton_numeric.filter.php
@@ -27,8 +27,8 @@ require_once(__DIR__ . '/filter.interface.php');
  */
 class stack_ast_filter_801_singleton_numeric implements stack_cas_astfilter_parametric {
 
-	// These two control the aceptable raw data types as well as 
-	// the mantissa allowed in the third option.
+    // These two control the aceptable raw data types as well as 
+    // the mantissa allowed in the third option.
     private $integer = true;
     private $float = true;
 
@@ -41,159 +41,167 @@ class stack_ast_filter_801_singleton_numeric implements stack_cas_astfilter_para
     private $convert = 'none'; // 'to float', 'to power'
 
     public function set_filter_parameters(array $parameters) {
-    	$this->integer = $parameters['integer'];
-    	$this->float = $parameters['float'];
-    	$this->power = $parameters['power'];
-    	$this->convert = $parameters['convert'];
+        $this->integer = $parameters['integer'];
+        $this->float = $parameters['float'];
+        $this->power = $parameters['power'];
+        $this->convert = $parameters['convert'];
     }
 
     public function filter(MP_Node $ast, array &$errors, array &$answernotes, stack_cas_security $identifierrules): MP_Node {
 
-    	// First unpack the $ast.
-    	$node = $ast;
-    	if ($node instanceof MP_Root) {
-    		$node = $node->items[0];
-    	}
-    	if ($node instanceof MP_Statement) {
-    		$node = $node->statement;
-    	}
-    	if ($node instanceof MP_PrefixOp && ($node->op === '-' || $node->op === '+')) {
-    		$node = $node->rhs;
-    	}
+        // First unpack the $ast.
+        $node = $ast;
+        if ($node instanceof MP_Root) {
+            $node = $node->items[0];
+        }
+        if ($node instanceof MP_Statement) {
+            $node = $node->statement;
+        }
+        if ($node instanceof MP_PrefixOp && ($node->op === '-' || $node->op === '+')) {
+            $node = $node->rhs;
+        }
 
         // Make sure that we have the full parentnode linkking.
         // Mainly relevant for test cases.
         $ast->callbackRecurse(null);
 
-    	// Trivial exits.
-    	if ($node instanceof MP_Float) {
-    		if ($this->float) {
-    			if ($this->convert !== 'to power') {
-	    			return $ast;
-	    		} else {
-	    			$replacement = $this->float_to_power($node);
-	    			if ($node->parentnode instanceof MP_PrefixOp && $replacement instanceof MP_Operation) {
-	    				// Move the prefix to its natural place.
-	    				$replacement->replace($replacement->lhs, new MP_PrefixOp($node->parentnode->op, $replacement->lhs));
-	    				$node->parentnode->parentnode->replace($node->parentnode, $replacement);
-	    			} else {
-	    				$node->parentnode->replace($node, $replacement);
-	    			}
-	    			return $ast;
-	    		}
-    		} else {
-    			$node->position['invalid'] = true;
+        // Trivial exits.
+        if ($node instanceof MP_Float) {
+            if ($this->float) {
+                // Turn floats that are to small or large to powers of ten.
+                $p = intval(explode('E', $node->toString())[1]);
+                
+                if ($p < 0) {
+                    $p = $p - strlen(explode('E', $node->toString())[0]);
+                } else {
+                    $p = $p + strlen(explode('E', $node->toString())[0]);
+                }
+                if ($this->convert !== 'to power' && ($p < 303 && $p > -303)) {
+                    return $ast;
+                } else {
+                    $replacement = $this->float_to_power($node);
+                    if ($node->parentnode instanceof MP_PrefixOp && $replacement instanceof MP_Operation) {
+                        // Move the prefix to its natural place.
+                        $replacement->replace($replacement->lhs, new MP_PrefixOp($node->parentnode->op, $replacement->lhs));
+                        $node->parentnode->parentnode->replace($node->parentnode, $replacement);
+                    } else {
+                        $node->parentnode->replace($node, $replacement);
+                    }
+                    return $ast;
+                }
+            } else {
+                $node->position['invalid'] = true;
                 $answernotes[] = 'Illegal_floats';
                 $errors[] = stack_string('Illegal_singleton_floats', ['forms' => $this->acceptable_forms()]);
-    			return $ast;
-    		}
-    	}
-    	if ($node instanceof MP_Integer) {
-    		if (!$this->integer) {
-    			$node->position['invalid'] = true;
-    			$answernotes[] = 'Illegal_integer';
+                return $ast;
+            }
+        }
+        if ($node instanceof MP_Integer) {
+            if (!$this->integer) {
+                $node->position['invalid'] = true;
+                $answernotes[] = 'Illegal_integer';
                 $errors[] = stack_string('Illegal_singleton_integer', ['forms' => $this->acceptable_forms()]);
-    		}
-    		return $ast;
-    	}
+            }
+            return $ast;
+        }
 
-    	// The non trivial bit of identifying a power form representation.
-    	// 10^1 is also one, i.e. matissa being 1 and omitted is to be noted.
-    	$m = null;
-    	$p = null;
-    	$sgn = false;
-    	if ($node instanceof MP_Operation && $node->op === '^') {
-    		if ($node->lhs instanceof MP_Integer && $node->lhs->value === 10) {
-    			$m = '1';
-    			if ($node->rhs instanceof MP_Integer) {
-    				$p = $node->rhs->value;
-    			} else if (($node->rhs instanceof MP_PrefixOp) && ($node->rhs->op === '-' || $node->rhs->op === '+') && ($node->rhs->rhs instanceof MP_Integer)) {
-    				$p = $node->rhs->rhs->value;
-    				if ($node->rhs->op === '-') {
-    					$p = -$p;
-    				}
-    			} else {
-	    			$node->position['invalid'] = true;
-	    			$answernotes[] = 'Illegal_power';
-	                $errors[] = stack_string('Illegal_singleton_power', ['forms' => $this->acceptable_forms()]);
-	                return $ast;	
-    			}
-    		} else {
-    			$node->position['invalid'] = true;
-    			$answernotes[] = 'Illegal_power';
+        // The non trivial bit of identifying a power form representation.
+        // 10^1 is also one, i.e. matissa being 1 and omitted is to be noted.
+        $m = null;
+        $p = null;
+        $sgn = false;
+        if ($node instanceof MP_Operation && $node->op === '^') {
+            if ($node->lhs instanceof MP_Integer && $node->lhs->value === 10) {
+                $m = '1';
+                if ($node->rhs instanceof MP_Integer) {
+                    $p = $node->rhs->value;
+                } else if (($node->rhs instanceof MP_PrefixOp) && ($node->rhs->op === '-' || $node->rhs->op === '+') && ($node->rhs->rhs instanceof MP_Integer)) {
+                    $p = $node->rhs->rhs->value;
+                    if ($node->rhs->op === '-') {
+                        $p = -$p;
+                    }
+                } else {
+                    $node->position['invalid'] = true;
+                    $answernotes[] = 'Illegal_power';
+                    $errors[] = stack_string('Illegal_singleton_power', ['forms' => $this->acceptable_forms()]);
+                    return $ast;    
+                }
+            } else {
+                $node->position['invalid'] = true;
+                $answernotes[] = 'Illegal_power';
                 $errors[] = stack_string('Illegal_singleton_power', ['forms' => $this->acceptable_forms()]);
                 return $ast;
-    		}
-    	}
-    	if ($node instanceof MP_Operation && $node->op === '*' && $node->rhs instanceof MP_Operation && $node->rhs->op === '^') {
-    		// The power first
-    		if ($node->rhs->lhs instanceof MP_Integer && $node->rhs->lhs->value === 10) {
-    			if ($node->rhs->rhs instanceof MP_Integer) {
-    				$p = $node->rhs->rhs->value;
-    			} else if ($node->rhs->rhs instanceof MP_PrefixOp &&
-    				($node->rhs->rhs->op === '-' || $node->rhs->rhs->op === '+') &&
-    				$node->rhs->rhs->rhs instanceof MP_Integer) {
-    				$p = $node->rhs->rhs->rhs->value;
-    				if ($node->rhs->rhs->op === '-') {
-    					$p = -$p;
-    				}
-    			} else {
-	    			$node->position['invalid'] = true;
-	    			$answernotes[] = 'Illegal_power';
-	                $errors[] = stack_string('Illegal_singleton_power', ['forms' => $this->acceptable_forms()]);
-	                return $ast;	
-    			}
-    		} else {
-    			$node->position['invalid'] = true;
-    			$answernotes[] = 'Illegal_power';
+            }
+        }
+        if ($node instanceof MP_Operation && $node->op === '*' && $node->rhs instanceof MP_Operation && $node->rhs->op === '^') {
+            // The power first
+            if ($node->rhs->lhs instanceof MP_Integer && $node->rhs->lhs->value === 10) {
+                if ($node->rhs->rhs instanceof MP_Integer) {
+                    $p = $node->rhs->rhs->value;
+                } else if ($node->rhs->rhs instanceof MP_PrefixOp &&
+                    ($node->rhs->rhs->op === '-' || $node->rhs->rhs->op === '+') &&
+                    $node->rhs->rhs->rhs instanceof MP_Integer) {
+                    $p = $node->rhs->rhs->rhs->value;
+                    if ($node->rhs->rhs->op === '-') {
+                        $p = -$p;
+                    }
+                } else {
+                    $node->position['invalid'] = true;
+                    $answernotes[] = 'Illegal_power';
+                    $errors[] = stack_string('Illegal_singleton_power', ['forms' => $this->acceptable_forms()]);
+                    return $ast;    
+                }
+            } else {
+                $node->position['invalid'] = true;
+                $answernotes[] = 'Illegal_power';
                 $errors[] = stack_string('Illegal_singleton_power', ['forms' => $this->acceptable_forms()]);
-                return $ast;	
-    		}
-    		// Then the mantissa
-    		if ($node->lhs instanceof MP_Integer) {
-    			$m = $node->lhs->raw;
-    			if (!$this->integer) {
-    			    $node->position['invalid'] = true;
-	    			$answernotes[] = 'Illegal_integer';
-	                $errors[] = stack_string('Illegal_singleton_integer', ['forms' => $this->acceptable_forms()]);
-	                return $ast;
-    			}
-    		} else if ($node->lhs instanceof MP_Float) {
-    			$m = $node->lhs->raw;
-    			if (!$this->float) {
-	    			$node->position['invalid'] = true;
-	                $answernotes[] = 'Illegal_floats';
-	                $errors[] = stack_string('Illegal_singleton_floats', ['forms' => $this->acceptable_forms()]);
-	    			return $ast;
-    			}
-    		} else if ($node->lhs instanceof MP_PrefixOp && ($node->lhs->op === '-' || $node->lhs->op === '+')) {
-    			if ($node->lhs->op === '-') {
-    				$sgn = '-';
-    			}
-    			if ($node->lhs->rhs instanceof MP_Integer) {
-    				$m = $node->lhs->rhs->raw;
-    				if (!$this->integer) {
-	    			    $node->position['invalid'] = true;
-		    			$answernotes[] = 'Illegal_integer';
-		                $errors[] = stack_string('Illegal_singleton_integer', ['forms' => $this->acceptable_forms()]);
-		                return $ast;
-	    			}
-    			} else if ($node->lhs->rhs instanceof MP_Float) {
-    				$m = $node->lhs->rhs->raw;
-	    			if (!$this->float) {
-		    			$node->position['invalid'] = true;
-		                $answernotes[] = 'Illegal_floats';
-		                $errors[] = stack_string('Illegal_singleton_floats', ['forms' => $this->acceptable_forms()]);
-		    			return $ast;
-	    			}
-    			} else {
-	    			$node->position['invalid'] = true;
-	    			$answernotes[] = 'Illegal_power';
-	                $errors[] = stack_string('Illegal_singleton_power', ['forms' => $this->acceptable_forms()]);
-	                return $ast;		
-    			}
-    		}
-		}
+                return $ast;    
+            }
+            // Then the mantissa
+            if ($node->lhs instanceof MP_Integer) {
+                $m = $node->lhs->raw;
+                if (!$this->integer) {
+                    $node->position['invalid'] = true;
+                    $answernotes[] = 'Illegal_integer';
+                    $errors[] = stack_string('Illegal_singleton_integer', ['forms' => $this->acceptable_forms()]);
+                    return $ast;
+                }
+            } else if ($node->lhs instanceof MP_Float) {
+                $m = $node->lhs->raw;
+                if (!$this->float) {
+                    $node->position['invalid'] = true;
+                    $answernotes[] = 'Illegal_floats';
+                    $errors[] = stack_string('Illegal_singleton_floats', ['forms' => $this->acceptable_forms()]);
+                    return $ast;
+                }
+            } else if ($node->lhs instanceof MP_PrefixOp && ($node->lhs->op === '-' || $node->lhs->op === '+')) {
+                if ($node->lhs->op === '-') {
+                    $sgn = '-';
+                }
+                if ($node->lhs->rhs instanceof MP_Integer) {
+                    $m = $node->lhs->rhs->raw;
+                    if (!$this->integer) {
+                        $node->position['invalid'] = true;
+                        $answernotes[] = 'Illegal_integer';
+                        $errors[] = stack_string('Illegal_singleton_integer', ['forms' => $this->acceptable_forms()]);
+                        return $ast;
+                    }
+                } else if ($node->lhs->rhs instanceof MP_Float) {
+                    $m = $node->lhs->rhs->raw;
+                    if (!$this->float) {
+                        $node->position['invalid'] = true;
+                        $answernotes[] = 'Illegal_floats';
+                        $errors[] = stack_string('Illegal_singleton_floats', ['forms' => $this->acceptable_forms()]);
+                        return $ast;
+                    }
+                } else {
+                    $node->position['invalid'] = true;
+                    $answernotes[] = 'Illegal_power';
+                    $errors[] = stack_string('Illegal_singleton_power', ['forms' => $this->acceptable_forms()]);
+                    return $ast;        
+                }
+            }
+        }
         // Ok so if we did not match before this or fail before this
         // then we should have the $m and $p and even $sgn if needed.
         // If conversion toward floats is needed we can do that
@@ -205,8 +213,9 @@ class stack_ast_filter_801_singleton_numeric implements stack_cas_astfilter_para
             $errors[] = stack_string('Illegal_singleton_power', ['forms' => $this->acceptable_forms()]);
             return $ast;        
         }
-        if ($this->convert === 'to float') {
-            $replacement = new MP_Float($m . 'e' . $p, floatval($m . 'e' . $p));
+        // Only convert to float if safe to do so.
+        if ($this->convert === 'to float' && (($p - strlen($m)) > -303 && ($p + strlen($m)) < 303)) {
+            $replacement = new MP_Float(floatval($m . 'e' . $p), $m . 'e' . $p);
             if ($sgn === '-') {
                 $replacement = new MP_PrefixOp('-', $replacement);
             }
@@ -225,25 +234,25 @@ class stack_ast_filter_801_singleton_numeric implements stack_cas_astfilter_para
     }
 
     private function acceptable_forms(): string {
-    	$r = [];
-    	if ($this->integer) {
-    		$r[] = '12345';
-    		$r[] = '-12345';
-    	}
-    	if ($this->float) {
-    		$r[] = '-1.2345';
-    		$r[] = '1.2E45';
-    		$r[] = '1.2e45';
-    	}
-    	if ($this->power) {
-			if ($this->integer) {
-				$r[] = '123*10^45';
-			}
-			if ($this->float) {
-				$r[] = '1.23*10^-45';
-			}
-    	}
-    	return implode(', ', $r);
+        $r = [];
+        if ($this->integer) {
+            $r[] = '12345';
+            $r[] = '-12345';
+        }
+        if ($this->float) {
+            $r[] = '-1.2345';
+            $r[] = '1.2E45';
+            $r[] = '1.2e45';
+        }
+        if ($this->power) {
+            if ($this->integer) {
+                $r[] = '123*10^45';
+            }
+            if ($this->float) {
+                $r[] = '1.23*10^-45';
+            }
+        }
+        return implode(', ', $r);
     }
 
     private function float_to_power(MP_Float $float): MP_Node {

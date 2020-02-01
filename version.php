cd1c9e68ff875e0bae2c3ec05bafc63c5db9511b
Chris Sangwin <C.J.Sangwin@gmail.com>
2020-02-01 08:39:05 +0000

    Bump dev version number ahead of master.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020020100;
+$plugin->version   = 2020020101;
 $plugin->requires  = 2018051700;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

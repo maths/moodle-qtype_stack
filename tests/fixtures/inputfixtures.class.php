03b35d6e72cc368f938651e40e5273aa5428176b
Chris Sangwin <C.J.Sangwin@gmail.com>
2020-06-04 18:01:34 +0100

    Add in numerous units-related test cases.

--- tests/fixtures/inputfixtures.class.php
+++ tests/fixtures/inputfixtures.class.php
@@ -482,34 +482,66 @@ class stack_inputvalidation_test_data {
             '\prod_{k=1}^{3}{\cos \left( k\cdot x \right)}', '', '')
     );
 
+    protected static $rawdata_units = array(
+        array('123', 'php_true', '123', 'cas_true', '123\,', 'Units_SA_no_units', "Units"),
+        array('9.81*m/s^2', 'php_true', '9.81*m/s^2', 'cas_true', '9.81\, {\mathrm{m}}/{\mathrm{s}^2}', '', ""),
+        array('9.81*m*s^-2', 'php_true', '9.81*m*s^-2', 'cas_true', '9.81\, {\mathrm{m}}/{\mathrm{s}^2}', '', ""),
+        array('2*(pi+1)', 'php_true', '2*(pi+1)', 'cas_true', '2\, \left(\pi+1\right)\,', 'Units_SA_no_units', ""),
+        array('(2*(pi+1))*mm', 'php_true', '(2*(pi+1))*mm', 'cas_true',
+            '2\, \left(\pi+1\right)\, \mathrm{m}\mathrm{m}', '', ""),
+        array('2*(pi+1)*mm', 'php_true', '2*(pi+1)*mm', 'cas_true',
+            '2\, \left(\pi+1\right)\, \mathrm{m}\mathrm{m}', '', ""),
+        array('2*mm*(pi+1)', 'php_true', '2*mm*(pi+1)', 'cas_true',
+            '2\, \left(\pi+1\right)\, \mathrm{m}\mathrm{m}', '', ""),
+        array('(2*mm)*(pi+1)', 'php_true', '(2*mm)*(pi+1)', 'cas_true',
+            '2\, \left(\pi+1\right)\, \mathrm{m}\mathrm{m}', '', ""),
+        array('mm*2*(pi+1)', 'php_true', 'mm*2*(pi+1)', 'cas_true', '2\, \left(\pi+1\right)\, \mathrm{m}\mathrm{m}', '', ""),
+        array('(pi+1)*2*mm', 'php_true', '(pi+1)*2*mm', 'cas_true', '\left(\pi+1\right)\, 2\, \mathrm{m}\mathrm{m}', '', ""),
+        array('(pi+1)*mm*2', 'php_true', '(pi+1)*mm*2', 'cas_true', '\left(\pi+1\right)\, 2\, \mathrm{m}\mathrm{m}', '', ""),
+        array('(2*1*(pi+1))*mm', 'php_true', '(2*1*(pi+1))*mm', 'cas_true',
+            '2\, 1\, \left(\pi+1\right)\, \mathrm{m}\mathrm{m}', '', ""),
+        array('(1*2*(pi+1))*mm', 'php_true', '(1*2*(pi+1))*mm', 'cas_true',
+            '1\, 2\, \left(\pi+1\right)\, \mathrm{m}\mathrm{m}', '', ""),
+        array('(2*pi+2)*mm', 'php_true', '(2*pi+2)*mm', 'cas_true',
+            '\left( 2\, \pi+2\right)\, \mathrm{m}\mathrm{m}', '', ""),
+        array('(2+0)*(pi+1)*mm', 'php_true', '(2+0)*(pi+1)*mm', 'cas_true',
+            '\left(2+0\right)\, \left(\pi+1\right)\, \mathrm{m}\mathrm{m}', '', ""),
+        array('(pi+1)*(2*mm)', 'php_true', '(pi+1)*(2*mm)', 'cas_true',
+            '\left(\pi+1\right)\, 2\, \mathrm{m}\mathrm{m}', '', ""),
+    );
+
     public static function get_raw_test_data() {
         return self::$rawdata;
     }
 
-    public static function test_from_raw($data) {
+    public static function test_from_raw($data, $validationmethod) {
 
         $test = new stdClass();
-        $test->rawstring     = $data[self::RAWSTRING];
-        $test->phpvalid      = $data[self::PHPVALID];
-        $test->phpcasstring  = $data[self::PHPCASSTRING];
-        $test->casvalid      = $data[self::CASVALID];
-        $test->display       = $data[self::DISPLAY];
-        $test->notes         = $data[self::NOTES];
-        $test->ansnotes      = $data[self::ANSNOTES];
+        $test->rawstring        = $data[self::RAWSTRING];
+        $test->phpvalid         = $data[self::PHPVALID];
+        $test->phpcasstring     = $data[self::PHPCASSTRING];
+        $test->casvalid         = $data[self::CASVALID];
+        $test->display          = $data[self::DISPLAY];
+        $test->notes            = $data[self::NOTES];
+        $test->ansnotes         = $data[self::ANSNOTES];
+        $test->validationmethod = $validationmethod;
 
-        $test->passed        = null;
-        $test->errors        = null;
-        $test->caserrors     = null;
-        $test->casdisplay    = null;
-        $test->casvalue      = null;
-        $test->casnotes      = null;
+        $test->passed           = null;
+        $test->errors           = null;
+        $test->caserrors        = null;
+        $test->casdisplay       = null;
+        $test->casvalue         = null;
+        $test->casnotes         = null;
         return $test;
     }
 
     public static function get_all() {
         $tests = array();
         foreach (self::$rawdata as $data) {
-            $tests[] = self::test_from_raw($data);
+            $tests[] = self::test_from_raw($data, 'typeless');
+        }
+        foreach (self::$rawdata_units as $data) {
+            $tests[] = self::test_from_raw($data, 'units');
         }
         return $tests;
     }
@@ -542,7 +574,7 @@ class stack_inputvalidation_test_data {
         $filterstoapply[] = '990_no_fixing_spaces';
 
         $cs = stack_ast_container::make_from_student_source($test->rawstring, '', new stack_cas_security(), $filterstoapply);
-        $cs->set_cas_validation_context('ans1', true, '', 'typeless', false);
+        $cs->set_cas_validation_context('ans1', true, '', $test->validationmethod, false);
 
         $phpvalid     = $cs->get_valid();
         $phpcasstring = $cs->get_inputform();

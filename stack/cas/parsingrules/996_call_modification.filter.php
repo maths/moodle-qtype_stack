2328b6b7e917ef4778567f64c6c3d55ff7a00dc6
Matti Harjula <matti.harjula@aalto.fi>
2022-01-27 10:17:18 +0200

    Make a filter truly indempotent.

--- stack/cas/parsingrules/996_call_modification.filter.php
+++ stack/cas/parsingrules/996_call_modification.filter.php
@@ -24,45 +24,76 @@ require_once(__DIR__ . '/filter.interface.php');
  */
 class stack_ast_filter_996_call_modification implements stack_cas_astfilter {
 
+    // The name of the function that checks identifiers.
+    const IDCHECK = '_C';
+
+    // The name of the function that checks expressions.
+    const EXPCHECK = '_CE';
+
     public function filter(MP_Node $ast, array &$errors, array &$answernotes, stack_cas_security $identifierrules): MP_Node {
 
         $mapfuns = stack_cas_security::get_all_with_feature('mapfunction');
         $process = function($node) use ($mapfuns) {
-            if ($node instanceof MP_Functioncall && !isset($node->position['997']) && !$node->is_definition()) {
-                if ($node->name instanceof MP_Atom && ($node->name->value === 'apply' || isset($mapfuns[$node->name->value]))) {
-                    // E.g. apply(foo,...) => (_C(foo),_C(apply),apply(foo,...)).
-                    $replacement = new MP_Group([new MP_FunctionCall(new MP_Identifier('_C'),
-                        [$node->arguments[0]]), new MP_FunctionCall(new MP_Identifier('_C'), [$node->name]), $node]);
-                    $replacement->items[0]->position['997'] = true;
-                    $replacement->items[1]->position['997'] = true;
-                    $replacement->items[2]->position['997'] = true;
-                    $node->parentnode->replace($node, $replacement);
-                    return false;
-                } else if ($node->name instanceof MP_Atom && ($node->name->value === 'at' || $node->name->value === 'subst')) {
-                    // E.g. subst([f=g],g(x)) => (_C(subst),_CE(subst([f=g],g(x)))).
-                    $replacement = new MP_Group([new MP_FunctionCall(new MP_Identifier('_C'), [$node->name]),
-                        new MP_Functioncall(new MP_Identifier('_CE'), [$node])]);
-                    $replacement->items[0]->position['997'] = true;
-                    $replacement->items[1]->position['997'] = true;
-                    $replacement->items[1]->arguments[0]->position['997'] = true;
-                    $node->parentnode->replace($node, $replacement);
-                    return false;
-                } else {
-                    // E.g. f(x) => (_C(f),f(x)).
-                    $replacement = new MP_Group([new MP_FunctionCall(new MP_Identifier('_C'), [$node->name]), $node]);
-                    $replacement->items[0]->position['997'] = true;
-                    // 'f(x) => (_C(f),'f(x))
-                    if ($node->parentnode instanceof MP_PrefixOp) {
-                        $replacement->items[1] = $node->parentnode;
-                        $replacement->items[1]->rhs->position['997'] = true;
-                        $node->parentnode->parentnode->replace($node->parentnode, $replacement);
-                    } else {
-                        $replacement->items[1]->position['997'] = true;
-                        $node->parentnode->replace($node, $replacement);
-                    }
+            if ($node instanceof MP_Functioncall) {
+                if ($node->name instanceof MP_Atom && ($node->name->value === self::IDCHECK || $node->name->value === self::EXPCHECK)) {
+                    // No checks for the checks themselves. They are protected using other means.
+                    return true;
+                }
+                $namecheck = new MP_FunctionCall(new MP_Identifier(self::IDCHECK), [$node->name]);
+                // The order of these ifs is critical, we build up the checks
+                // so that no basic check gets lost due to more advanced ones
+                // doing more conplex things. The advanced cases assume that 
+                // the simpler ones have been done already.
+                if (!($node->parentnode instanceof MP_Group) || $node->parentnode->items[0]->toString() !== $namecheck->toString()) {
+                    $replacement = new MP_Group([$namecheck, $node]);
+                    if ($node->parentnode instanceof MP_PrefixOp || ($node->parentnode instanceof MP_FunctionCall && $node->parentnode->name instanceof MP_Atom && $node->parentnode->name->value === self::EXPCHECK)) {
+                        // _CE(subst(...)) => (_C(subst),_CE(subst(...)))
+                        // 'f(x) => (_C(f),'f(x))
+                        // This needs to be indempotent.
+                        if ($node->parentnode->parentnode instanceof MP_Group && $node->parentnode->parentnode->items[0]->toString() === $replacement->items[0]->toString()) {
 
+                        } else {
+                            $replacement->items[1] = $node->parentnode;
+                            $node->parentnode->parentnode->replace($node->parentnode, $replacement);
+                            return false;
+                        }
+                    } else {
+                        // f(x) => (_C(f),f(x))
+                        $node->parentnode->replace($node, $replacement);
+                        return false;
+                    }
+                }
+                if ($node->name instanceof MP_Atom && $node->name->value === 'ev' && count($node->arguments) > 0) {
+                    if (!($node->arguments[0] instanceof MP_FunctionCall) || !($node->arguments[0]->name instanceof MP_Atom) ||
+                        $node->arguments[0]->name->value !== self::EXPCHECK) {
+                        // ev(foo, ...) => ev(_CE(foo),...)
+                        $node->replace($node->arguments[0], new MP_FunctionCall(new MP_Identifier(self::EXPCHECK), [$node->arguments[0]]));
+                        return false;
+                    }
+                    return true;
+                }
+                if ($node->name instanceof MP_Atom && ($node->name->value === 'subst' || $node->name->value === 'at')) {
+                    // subst(...) => _CE(subst(...)), always even when
+                    // we check again at eval time.
+                    if ($node->parentnode instanceof MP_FunctionCall && $node->parentnode->name instanceof MP_Atom && $node->parentnode->name->value === self::EXPCHECK) {
+                        return true;
+                    }
+                    $node->parentnode->replace($node, new MP_FunctionCall(new MP_Identifier(self::EXPCHECK), [$node]));
                     return false;
                 }
+                if ($node->name instanceof MP_Atom && ($node->name->value === 'apply' || isset($mapfuns[$node->name->value]))) {
+                    // E.g. apply(foo,...) => (_C(foo),_C(apply),apply(foo,...)).
+                    $check = new MP_FunctionCall(new MP_Identifier(self::IDCHECK), [$node->arguments[0]]);
+                    if ($node->parentnode->items[1]->toString() !== $check->toString()) {
+                        $node->parentnode->items = array_merge([$node->parentnode->items[0], $check], array_slice($node->parentnode->items, 1));
+                        return false;
+                    }
+                }
+            }
+            if ($node instanceof MP_PrefixOp && $node->op === "''" && !($node->rhs instanceof MP_FunctionCall && $node->rhs->name instanceof MP_Atom && $node->rhs->name->value === self::EXPCHECK)) {
+                // ''x => ''_CE(x)
+                $node->replace($node->rhs, new MP_FunctionCall(new MP_Identifier(self::EXPCHECK), [$node->rhs]));
+                return false;
             }
             return true;
         };

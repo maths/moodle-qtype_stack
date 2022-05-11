a5682637cd73da94b81ae900ee94678f090ef1c9
Matti Harjula <aharjula@konsta.hut.fi>
2022-05-11 11:06:45 +0300

    Add parameter handling to [[commonstring]] so that it is  simpelr to do preprocessing of the values. The block by defautl assuems that the parameter value should go through "{@...@}" but one can use specific prefixes to tune that and the simplification rules and if that is not enough simply give the parameters as strings for full control.

--- stack/cas/castext2/blocks/commonstring.block.php
+++ stack/cas/castext2/blocks/commonstring.block.php
@@ -18,14 +18,70 @@ defined('MOODLE_INTERNAL') || die();
 require_once(__DIR__ . '/../block.interface.php');
 require_once(__DIR__ . '/../../../utils.class.php');
 
-
+/**
+ * The commonstring block is used to ouput string templates that
+ * come from the localised strings. It requires that one defined 
+ * the key of the template and allows one to declare named parameters
+ * to be placed into it. When declaring named parameters one can prefix 
+ * the parameter name with one of these strings:
+ *
+ *   "" or no prefix will just act like {@...@} but will follow 
+ *      the local simplification setting
+ *   "nosimp_" will do normal {@...@} rendering but without
+ *      simplification.
+ *
+ *   "raw_" will cause the value to be pushed through `string()`
+ *      like in the use of {#...#} with the local simplification.
+ *
+ *   "nosimp_raw_" will cause the value to be pushed through `string()`
+ *      like in the use of {#...#} without simplification.
+ *
+ */
 class stack_cas_castext2_commonstring extends stack_cas_castext2_block {
 
     public function compile($format, $options): ?string {
         // The user should use this blocks full name "commonstring" but
         // as this is a common block and chars take room we tend to use a shorter
         // one internally "%cs", note that the processors need to know of this.
-        return '["%cs",' . stack_utils::php_string_to_maxima_string($this->params['key']) . ']';
+        if (count($this->params) == 1) {
+            return '["%cs",' . stack_utils::php_string_to_maxima_string($this->params['key']) . ']';
+        } else {
+            $r = ['["%cs"',stack_utils::php_string_to_maxima_string($this->params['key'])];
+
+            $epos = $options['context'] . '/' . $this->position['start'] . '-' . $this->position['end'];
+            $epos = stack_utils::php_string_to_maxima_string($epos);
+
+            foreach ($this->params as $key => $value) {
+                if ($key !== 'key') {
+                    $ev = stack_ast_container::make_from_teacher_source($value);
+                    $ev = $ev->get_evaluationform();
+                    $ev = "_EC(errcatch(_ct2_tmp:$ev),$epos)";
+
+
+                    if (strpos($key, 'nosimp_raw_') === 0) {
+                        $r[] = stack_utils::php_string_to_maxima_string(mb_substr($key, 11));
+                        $r[] = 'block([_ct2_tmp,_ct2_simp],_ct2_simp:simp,simp:false,' . $ev .
+                ',_ct2_tmp:string(_ct2_tmp),simp:_ct2_simp,_ct2_tmp)';
+                    } else if (strpos($key, 'nosimp_') === 0) {
+                        $r[] = stack_utils::php_string_to_maxima_string(mb_substr($key, 7));
+                        $r[] = 'block([_ct2_tmp,_ct2_simp],_ct2_simp:simp,simp:false,' . $ev . ',simp:false,_ct2_tmp:ct2_latex(_ct2_tmp,"i"),simp:_ct2_simp,_ct2_tmp)';
+                    } else if (strpos($key, 'raw_') === 0) {
+                        $r[] = stack_utils::php_string_to_maxima_string(mb_substr($key, 4));
+                        // If prefixed by raw output as {#...#} would do.
+                        $r[] = 'block([_ct2_tmp],' . $ev . ',string(_ct2_tmp))';
+                    } else {
+                        // By default assume the value is to be handled like {@...@} would handle it.
+                        $r[] = stack_utils::php_string_to_maxima_string($key);
+
+                        $r[] = 'block([_ct2_tmp,_ct2_simp],_ct2_simp:simp,' . $ev . ',simp:false,_ct2_tmp:ct2_latex(_ct2_tmp,"i"),simp:_ct2_simp,_ct2_tmp)';
+                    }
+                }
+            }
+
+
+            $r[count($r)-1] .= ']';
+            return implode(',', $r);
+        }
     }
 
     public function is_flat(): bool {
@@ -33,10 +89,42 @@ class stack_cas_castext2_commonstring extends stack_cas_castext2_block {
     }
 
     public function validate_extract_attributes(): array {
-        return array();
+        $r = array();
+        foreach ($this->params as $key => $value) {
+            $r[] = stack_ast_container_silent::make_from_teacher_source(
+                $value, 'ct2:commonstring', new stack_cas_security());
+        }
+        return $r;
+
     }
 
     public function postprocess(array $params, castext2_processor $processor): string {
-        return stack_string($params[1]);
+        if (count($params) === 2) {
+            return stack_string($params[1]);
+        }
+        $args = [];
+        for ($i = 2; $i < count($params); $i += 2) {
+            $val = '';
+            if (is_array($params[$i+1])) {
+                $val = $processor->process($params[$i+1][0], $params[$i+1]);
+            } else {
+                $val = $params[$i+1];
+            }
+
+            $args[$params[$i]] = $val;
+        }
+
+        return stack_string($params[1], $args);
+    }
+
+
+    public function validate(&$errors=array(), array $options): bool {
+        if (!array_key_exists('key', $this->params)) {
+            $errors[] = new $options['errclass']('The commonstring block must always have a key for the string template.',
+                $options['context'] . '/' . $this->position['start'] . '-' . $this->position['end']);
+            return false;
+        }
+
+        return true;
     }
 }

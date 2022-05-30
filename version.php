887e5dfbc600c84a2ac514f7ab7a485098d554d4
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2022-05-30 16:26:30 +0100

    Bump version number.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022053000;
+$plugin->version   = 2022053001;
 $plugin->requires  = 2018051700;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

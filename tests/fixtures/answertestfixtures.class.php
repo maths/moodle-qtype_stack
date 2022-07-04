cbb0471f42b7cbe80163b84a9d41e4d019d3e647
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2022-07-04 18:19:03 +0100

    Improve dealing with inequalities containing inf and minf.

--- tests/fixtures/answertestfixtures.class.php
+++ tests/fixtures/answertestfixtures.class.php
@@ -371,6 +371,23 @@ class stack_answertest_test_data {
         array('AlgEquiv', '', '%union(oo(-inf,1),oo(-1,inf))', 'oo(-inf,inf)', 1, 'ATRealSet_true.', ''),
         array('AlgEquiv', '', 'all', 'oo(-inf,inf)', 1, 'ATRealSet_true.', ''),
 
+        // TODO: fix type checking to allow these examples.
+        array('AlgEquiv', '', 'co(1,2)', '1 <= x nounand x<2', 0, 'ATAlgEquiv_SA_not_logic.', ''),
+        array('AlgEquiv', '', '1 <= x nounand x<2', 'co(1,2)', 0, 'ATAlgEquiv_SA_not_realset.', ''),
+        array('AlgEquiv', '', 'minf <= x', 'co(minf,inf)', 0, 'ATAlgEquiv_SA_not_realset.', ''),
+        array('AlgEquiv', '', '-inf <= x', 'co(minf,inf)', 0, 'ATAlgEquiv_SA_not_realset.', ''),
+        array('AlgEquiv', '', 'x <= inf', 'oc(minf,inf)', 0, 'ATAlgEquiv_SA_not_realset.', ''),
+        array('AlgEquiv', '', 'minf <= x', 'oo(minf,inf)', 0, 'ATAlgEquiv_SA_not_realset.', ''),
+        // So for now.
+        array('AlgEquiv', '', 'single_variable_solver_real(minf <= x)',
+            'co(minf,inf)', 1, 'ATRealSet_true.', ''),
+        array('AlgEquiv', '', 'single_variable_solver_real(-inf <= x)',
+            'co(minf,inf)', 1, 'ATRealSet_true.', ''),
+        array('AlgEquiv', '', 'single_variable_solver_real(x <= inf)',
+            'oc(minf,inf)', 1, 'ATRealSet_true.', ''),
+        array('AlgEquiv', '', 'single_variable_solver_real(minf <= x)',
+            'oo(minf,inf)', 0, 'ATRealSet_false.', ''),
+
         array('AlgEquiv', '', 'a=b/%i', '%i*a=b', 1, 'ATEquation_num_i', 'Complex numbers'),
         array('AlgEquiv', '', 'b/%i=a', '%i*a=b', 1, 'ATEquation_num_i', ''),
         array('AlgEquiv', '', 'b=a/%i', '%i*a=b', 0, 'ATEquation_lhs_notrhs_op', ''),
@@ -452,9 +469,17 @@ class stack_answertest_test_data {
         array('AlgEquiv', '', 'x^6<1', 'abs(x)<1', -3, '', ''),
         array('AlgEquiv', '', 'abs(x)>1', 'x<-1 or x>1', -3, '', ''),
 
-        array('AlgEquiv', '', 'minf < x', 'minf <= x', 1, '', ''),
+        array('AlgEquiv', '', 'minf < x', 'minf <= x', 0, 'ATInequality_strict.', ''),
+        array('AlgEquiv', '', 'x>minf', 'minf < x', 1, '', ''),
+        array('AlgEquiv', '', 'x>-inf', 'minf < x', 1, '', ''),
+        // Because Maxima does not simplify expressions like 2*inf->inf!
+        array('AlgEquiv', '', 'x<2*inf', 'x<inf', 0, '', ''),
         array('AlgEquiv', '', 'minf < x nounand x <1', 'x<1', 1, '', ''),
         array('AlgEquiv', '', 'minf < x nounand x <1', 'x<2', 0, '', ''),
+
+        // Please note the following, which are a Maxima design issue!
+        array('AlgEquiv', '', '2*inf', 'inf', 0, '', 'Maxima and infinity'),
+        array('AlgEquiv', '', '-inf', 'minf', 0, '', ''),
 
         array('AlgEquiv', '', 'x#1', 'x#1', 1, 'ATLogic_True.', 'Not equal to'),
         array('AlgEquiv', '', 'x#(1+1)', 'x#2', 1, 'ATLogic_True.', ''),

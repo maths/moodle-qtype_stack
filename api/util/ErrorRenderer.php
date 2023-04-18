d2c67dda47ad4a49b615b054909851d2a55f1c62
Sven Kirschbaum <kirschbaum@medien.rwth-aachen.de>
2023-04-18 11:50:08 +0200

    :goal_net: Hide Error Details for non STACK Exceptions

--- api/util/ErrorRenderer.php
+++ api/util/ErrorRenderer.php
@@ -8,8 +8,9 @@ class ErrorRenderer implements ErrorRendererInterface
 {
     public function __invoke(\Throwable $exception, bool $displayErrorDetails): string
     {
+        $message = $exception instanceof \stack_exception ? $exception->getMessage() : "An Error occured while processing the question";
         return json_encode(array(
-            'message' => $exception->getMessage()
+            'message' => $message
         ));
     }
 

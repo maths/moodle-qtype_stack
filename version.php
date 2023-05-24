10043290af0462339b9abc99ac8f6de7f4174886
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2023-05-24 19:33:06 +0100

    Bump dev branch version ahead of master.

--- version.php
+++ version.php
@@ -24,12 +24,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023052400;
+$plugin->version   = 2023052500;
 $plugin->requires  = 2020061500;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '4.4.3 for Moodle 3.9+';
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '4.4.4 for Moodle 3.9+';
 
 $plugin->dependencies = array(
     'qbehaviour_adaptivemultipart'     => 2020103000,

dc9212a055f21c59294296422d0accaeff6826cb
Work Ac <yassmin.mohandis@gmail.com>
2023-06-29 08:36:52 +0200

    OK

--- stack/cas/castext2/blocks/adaptbutton.block.php
+++ stack/cas/castext2/blocks/adaptbutton.block.php
@@ -46,7 +46,7 @@ class stack_cas_castext2_adaptbutton extends stack_cas_castext2_block {
 
 
         $code = 'import {stack_js} from "' . stack_cors_link('stackjsiframe.min.js') . '";';
-        //$code .= 'stack_js.request_access_to_input("' . $this->params['input'] . '", true).then((id) => {';
+        $code .= 'stack_js.request_access_to_input("' . $this->params['input'] . '", true).then((id) => {';
         // So that should give us access to the input.
         // Once we get the access immediately bind a listener to it.
         $code .= 'const butt = document.getElementById("stack-adaptbutton-'.self::$countadaptbuttons.'");';

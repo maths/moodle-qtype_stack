06f786217369d99b1c921238b06a266a463a044d
mohandis <yassmin.mohandis@gmail.com>
2023-07-04 23:45:03 +0200

    iframe for adaptblock

--- stack/cas/castext2/blocks/adaptbutton.block.php
+++ stack/cas/castext2/blocks/adaptbutton.block.php
@@ -51,7 +51,7 @@ class stack_cas_castext2_adaptbutton extends stack_cas_castext2_block {
         $body->items[] = new MP_String('<button type="button" class="btn btn-secondary" id="stack-adaptbutton-' . 
             self::$countadaptbuttons . '">' . $this->params['title'] . '</button>');
 
-        $code = 'import {stack_js} from "' . stack_cors_link('stackjsiframe.min.js') . '";';
+        $code = 'import {stack_js} from "' . stack_cors_link('stackjsiframe.js') . '";';
         $code .= 'stack_js.request_access_to_button("stack-adaptbutton-' .self::$countadaptbuttons. '", true).then((id) => {';
         // So that should give us access to the input.
         // Once we get the access immediately bind a listener to it.

1e8cd715621b0a3865e3dfb6efee8e8be820e47e
mohandis <yassmin.mohandis@gmail.com>
2023-08-03 17:13:24 +0200

    setting checkbox true after button onclick

--- stack/cas/castext2/blocks/adaptbutton.block.php
+++ stack/cas/castext2/blocks/adaptbutton.block.php
@@ -28,8 +28,7 @@ class stack_cas_castext2_adaptbutton extends stack_cas_castext2_block {
 
         $body = new MP_List([new MP_String('%root')]);
 
-        $onclick = "const checkbox-button = document.getElementById('checkbox-adaptbutton-".self::$countadaptbuttons."');";
-        $onclick = "checkbox-button.checked = true;";
+        $onclick = "document.getElementById('checkbox-adaptbutton-".self::$countadaptbuttons."').checked=true";
         $onclick = "console.log(checkbox-button.checked);";
         if (isset($this->params['show_ids'])) {
             $split_show_id = preg_split ("/[\ \n\;]+/", $this->params['show_ids']); 

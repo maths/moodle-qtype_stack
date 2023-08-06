deecdd4786d479e073666af12b975f97a6888afa
mohandis <yassmin.mohandis@gmail.com>
2023-08-06 14:48:34 +0200

    'newline' in function

--- stack/cas/castext2/blocks/adaptbutton.block.php
+++ stack/cas/castext2/blocks/adaptbutton.block.php
@@ -62,15 +62,15 @@ class stack_cas_castext2_adaptbutton extends stack_cas_castext2_block {
         $body->items[] = new MP_String('<button type="button" class="btn btn-secondary" id="stack-adaptbutton-' . 
         self::$countadaptbuttons . '" onclick="' . $onclick . '" >' . $this->params['title'] . '</button>');
 
-        $code = 'function is_clicked(){ \n';
-        // $code .= 'const button = document.getElementById("stack-adaptbutton-' . self::$countadaptbuttons .'"); \n';
-        $code .= 'import {stack_js} from "' . stack_cors_link('stackjsiframe.min.js') . '"; \n';
-        $code .= 'stack_js.request_access_to_input("' . $this->params['save_state'] . '", true).then((id) => { \n';
-        $code .= 'const input = document.getElementById(id); \n';
-        // $code .= 'button.addEventListener("click",(e)=>{\n';
-        $code .= 'input.value="true"; \n}); \n';
-        $code .= '}';
-    
+        $code = "function is_clicked(){ \n";
+        // $code .= 'const button = document.getElementById("stack-adaptbutton-' . self::$countadaptbuttons .'");' . "\n";
+        $code .= "import {stack_js} from ". stack_cors_link('stackjsiframe.min.js')."; \n";
+        $code .= "stack_js.request_access_to_input('" . $this->params['save_state'] . "', true).then((id) => {\n";
+        $code .= "const input = document.getElementById(id); \n";
+        // $code .= 'button.addEventListener("click",(e)=>{' . "\n";
+        $code .= "input.value='true'; \n}); \n";
+        $code .= "}" ;
+            
         $body->items[] = new MP_String('<script>'.$code.'</script>');
 
         // //Now add a hidden [[iframe]] with suitable scripts.

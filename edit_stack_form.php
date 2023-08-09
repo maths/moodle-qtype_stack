d72feafc43562a83b4f1463acaa413ee8ad8c66b
mohandis <yassmin.mohandis@gmail.com>
2023-08-09 14:45:03 +0200

    save displaytype and buttontitles in input question

--- edit_stack_form.php
+++ edit_stack_form.php
@@ -698,9 +698,8 @@ class qtype_stack_edit_form extends question_edit_form {
             $question->{$inputname . 'modelans'}           = $input->tans;
             $question->{$inputname . 'boxsize'}            = $input->boxsize;
             // TODO: remove this when we delete it from the DB.
-            //changed
-            //$question->{$inputname . 'displaytype'}        = $input->displaytype;
-            //$question->{$inputname . 'buttontitles'}       = $input->buttontitles;
+            $question->{$inputname . 'displaytype'}        = $input->displaytype;
+            $question->{$inputname . 'buttontitles'}       = $input->buttontitles;
             $question->{$inputname . 'strictsyntax'}       = true;
             $question->{$inputname . 'insertstars'}        = $input->insertstars;
             $question->{$inputname . 'syntaxhint'}         = $input->syntaxhint;

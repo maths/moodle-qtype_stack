3bcbb983d67de38e2fea10e73c3587ef6b2ffb9d
mohandis <yassmin.mohandis@gmail.com>
2023-08-10 15:04:01 +0200

    changed the string for onclick

--- stack/input/boolean/boolean.class.php
+++ stack/input/boolean/boolean.class.php
@@ -111,13 +111,13 @@ class stack_boolean_input extends stack_input {
                 $attributes['id'] = $element_button_id;
                 $attributes['class'] = 'stack-input-toggle-button';
                 $attributes['type'] = 'checkbox';
-                $attributes['onclick'] = '
-                    if (document.getElementsByName("' . $fieldname . '")[0].value=="true") {
-                        document.getElementsByName("' . $fieldname . '")[0].value = "false";
+                $attributes['onclick'] = "
+                    if (document.getElementsByName('" . $fieldname . "')[0].value=='true') {
+                        document.getElementsByName('" . $fieldname . "')[0].value = 'false';
                     } else {
-                        document.getElementsByName("' . $fieldname . '")[0].value = "true";
+                        document.getElementsByName('" . $fieldname . "')[0].value = 'true';
                     };
-                ';
+                ";
                 //$element_button = html_writer::tag('input', "<span class='slider round'></span>", $attributes);
                 //Toggle-Button with Text
                 $element_button = html_writer::tag('input', "<span class='slider'></span><span class='slider-labels' data-on='True' data-off='False'></span>", $attributes);

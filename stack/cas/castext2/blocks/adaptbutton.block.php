ea567e1584e698657f878ba6d0b2b4c333293b1c
mohandis <yassmin.mohandis@gmail.com>
2023-08-17 20:11:56 +0200

    changing the button id

--- stack/cas/castext2/blocks/adaptbutton.block.php
+++ stack/cas/castext2/blocks/adaptbutton.block.php
@@ -83,7 +83,7 @@ class stack_cas_castext2_adaptbutton extends stack_cas_castext2_block {
         $code .= "input.dispatchEvent(new Event('change'));\n";
         $code .= "});\n";
 
-        $code .= "stack_js.request_access_to_button('adaptbutton-". self::$countadaptbuttons . "', true).then((id) => {\n";
+        $code .= "stack_js.request_access_to_button('stack-adaptbutton-". self::$countadaptbuttons . "', true).then((id) => {\n";
         $code .= "const button = document.getElementById(id);\n";
         $code .= "button.addEventListener('click',(e)=>{\n";
         $code .= "console.log('iframe funktioniert');\n});\n";

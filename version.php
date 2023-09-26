6deef3c2dca26be282fcbcade99fd40fda6aeae2
mohandis <yassmin.mohandis@gmail.com>
2023-09-27 00:20:12 +0200

    also version number

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023082400;
+$plugin->version   = 2023082400; 
 $plugin->requires  = 2020061500;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';

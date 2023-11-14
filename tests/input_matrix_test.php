c436aa420951b8fc978608cf64c766d6297dbc9d
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2023-11-14 17:47:25 +0000

    Fix to issue #1057: syntax hint in matrix inputs.

--- tests/input_matrix_test.php
+++ tests/input_matrix_test.php
@@ -120,6 +120,28 @@ class input_matrix_test extends qtype_stack_testcase {
                         'ans1', false, null));
     }
 
+    public function test_render_syntax_hint_spaces() {
+        $options = new stack_options();
+        $el = stack_input_factory::make('matrix', 'ans1', 'M', $options);
+        $el->set_parameter('syntaxHint', 'matrix([a,b], [?,d])');
+        $el->adapt_to_model_answer('matrix([1,0],[0,1])');
+        $this->assertEquals('<div class="matrixsquarebrackets"><table class="matrixtable" id="ans1_container" ' .
+            'style="display:inline; vertical-align: middle;" cellpadding="1" cellspacing="0"><tbody><tr>' .
+            '<td style="padding-top: 0.5em">&nbsp;</td>' .
+            '<td><input type="text" id="ans1_sub_0_0" name="ans1_sub_0_0" value="a" size="5" ' .
+            'autocapitalize="none" spellcheck="false"></td>' .
+            '<td><input type="text" id="ans1_sub_0_1" name="ans1_sub_0_1" value="b" size="5" ' .
+            'autocapitalize="none" spellcheck="false"></td>' .
+            '<td style="padding-top: 0.5em">&nbsp;</td></tr><tr><td>&nbsp;</td>' .
+            '<td><input type="text" id="ans1_sub_1_0" name="ans1_sub_1_0" value="?" size="5" ' .
+            'autocapitalize="none" spellcheck="false"></td>' .
+            '<td><input type="text" id="ans1_sub_1_1" name="ans1_sub_1_1" value="d" size="5" ' .
+            'autocapitalize="none" spellcheck="false"></td>' .
+            '<td style="padding-bottom: 0.5em">&nbsp;</td></tr></tbody></table></div>',
+            $el->render(new stack_input_state(stack_input::VALID, array(), '', '', '', '', ''),
+                'ans1', false, null));
+    }
+
     public function test_render_syntax_hint_round() {
         $options = new stack_options();
         $options->set_option('matrixparens', '(');

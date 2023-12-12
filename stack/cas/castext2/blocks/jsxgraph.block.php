1e38b12312c7fb1a836c7ac675efa55bb65ef0a2
smmercuri <smercuri@ed.ac.uk>
2023-12-12 13:57:51 +0000

    Bug fix to correct jsxgraph collapsing in height by changing 100% to 100vh in l.124

--- stack/cas/castext2/blocks/jsxgraph.block.php
+++ stack/cas/castext2/blocks/jsxgraph.block.php
@@ -121,13 +121,13 @@ class stack_cas_castext2_jsxgraph extends stack_cas_castext2_block {
             $height = $xpars['height'];
         }
 
-        $astyle = "width:calc(100% - 3px);height:calc(100% - 3px);";
+        $astyle = "width:calc(100% - 3px);height:calc(100vh - 3px);";
 
         if (array_key_exists('aspect-ratio', $xpars)) {
             $aspectratio = $xpars['aspect-ratio'];
             // Unset the undefined dimension, if both are defined then we have a problem.
             if (array_key_exists('height', $xpars)) {
-                $astyle = "height:calc($100% - 3px);aspect-ratio:$aspectratio;";
+                $astyle = "height:calc(100% - 3px);aspect-ratio:$aspectratio;";
             } else if (array_key_exists('width', $xpars)) {
                 $astyle = "width:calc(100% - 3px);aspect-ratio:$aspectratio;";
             }

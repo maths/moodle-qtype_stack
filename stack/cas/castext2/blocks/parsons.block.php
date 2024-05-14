ec3eaf6201b2314465bae124abec87a24efb61f1
smmercuri <smercuri@ed.ac.uk>
2024-05-14 10:08:51 +0100

    Rename third-party sortable lib to sortablecore and add to thirdpartylibs file

--- stack/cas/castext2/blocks/parsons.block.php
+++ stack/cas/castext2/blocks/parsons.block.php
@@ -195,7 +195,7 @@ class stack_cas_castext2_parsons extends stack_cas_castext2_block {
         $r->items[] = new MP_String('<script type="module">');
 
         $importcode = "\nimport {stack_js} from '" . stack_cors_link('stackjsiframe.min.js') . "';\n";
-        $importcode .= "import {Sortable} from '" . stack_cors_link('sortable.min.js') . "';\n";
+        $importcode .= "import {Sortable} from '" . stack_cors_link('sortablecore.min.js') . "';\n";
         $importcode .= "import {preprocess_steps,
                                 stack_sortable,
                                 get_iframe_height,

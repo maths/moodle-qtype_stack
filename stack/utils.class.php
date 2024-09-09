3c2f2c3282f181b22a8609d224e5ba63d836dc4b
smmercuri <smercuri@ed.ac.uk>
2024-09-09 13:17:47 +0100

    Refactor to avoid circular import

--- stack/utils.class.php
+++ stack/utils.class.php
@@ -1005,4 +1005,55 @@ class stack_utils {
         }
         return $arr;
     }
+
+    /**
+     * Takes a string that contains a list where each element has the format
+     * [<JSON>, <int>]
+     * and each JSON has the format
+     * {"used" : [[[<hashed string>, ..., <hashed string>]]], "available" : [<hashed_string>, ... <hashed_string>]}
+     * each `<hashed_string>` is assumed to be Base64-hashed.
+     * 
+     * This function will return the same format string, with each `<hashed_string>` replaced by the original string value.
+     */
+    public static function unhash_parsons_string($list_of_jsons) {
+        $decoded_list = json_decode($list_of_jsons);
+        foreach($decoded_list as $key => $json) {
+            $decoded_list[$key][0]->used[0][0] = stack_utils::unhash_array($decoded_list[$key][0]->used[0][0]);
+            $decoded_list[$key][0]->available = stack_utils::unhash_array($decoded_list[$key][0]->available);
+        }
+        return json_encode($decoded_list);
+    }
+
+        /**
+     * Maxima string version of `unhash_parsons_string`.
+     */
+    public static function unhash_parsons_string_maxima($list_of_jsons) {
+        $php_list_of_jsons = stack_utils::maxima_string_to_php_string($list_of_jsons);
+        return stack_utils::php_string_to_maxima_string(self::unhash_parsons_string($php_list_of_jsons));
+    }
+
+    /**
+     * Takes a string that contains a list where each element has the format
+     * [<JSON>, <int>]
+     * and each JSON has the format
+     * {"used" : [[[<string>, ..., <string>]]], "available" : [<string>, ... <string>]}
+     * 
+     * This function will return the same format string, with each `<string>` replaced by its Base64-hashed value.
+     */
+    public static function hash_parsons_string($list_of_jsons) {
+        $decoded_list = json_decode($list_of_jsons);
+        foreach($decoded_list as $key => $json) {
+            $decoded_list[$key][0]->used[0][0] = stack_utils::hash_array($decoded_list[$key][0]->used[0][0]);
+            $decoded_list[$key][0]->available = stack_utils::hash_array($decoded_list[$key][0]->available);
+        }
+        return json_encode($decoded_list);
+    }
+
+    /**
+     * Maxima string version of `hash_parsons_string`.
+     */
+    public static function hash_parsons_string_maxima($list_of_jsons) {
+        $php_list_of_jsons = stack_utils::maxima_string_to_php_string($list_of_jsons);
+        return stack_utils::php_string_to_maxima_string(self::hash_parsons_string($php_list_of_jsons));
+    }
 }

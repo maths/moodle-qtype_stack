868c615c1db9b7e0d3f1a01481750c305ddbf855
Edmund Farrow <edmund.farrow@ed.ac.uk>
2024-12-18 10:13:42 +0000

    code-tidy - Minor tweaks

--- stack/input/inputbase.class.php
+++ stack/input/inputbase.class.php
@@ -17,7 +17,7 @@
 /**
  * Add description here!
  * @package    qtype_stack
- * @copyright  2024 University of Edinburgh.
+ * @copyright  2012 University of Birmingham
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
  */
 
@@ -34,9 +34,6 @@ require_once(__DIR__ . '/inputstate.class.php');
  * Inputs are the controls that the teacher can put into the question
  * text to receive the student's response.
  *
- * @package    qtype_stack
- * @copyright  2012 University of Birmingham
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 abstract class stack_input {
     // phpcs:ignore moodle.Commenting.MissingDocblock.Constant

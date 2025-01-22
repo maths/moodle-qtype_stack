a695a3d036ade83774ba1b7fdafac6829af32439
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2025-01-22 15:19:54 +0000

    Up timeout further, and expose timeout value in err message (WIP for CI).

--- stack/cas/installhelper.class.php
+++ stack/cas/installhelper.class.php
@@ -464,7 +464,7 @@ END;
                 $ts->instantiate();
                 if (!$ts->get_valid() || $cs->get_value() != '2') {
                     $errors = $ts->get_errors();
-                    $errmsg = "Evaluation test failed, errors: $errors";
+                    $errmsg = "Evaluation test failed, errors: $errors . " . stack_utils::get_config()->castimeout;
                 } else {
                     // It worked!
                     $errmsg = '';

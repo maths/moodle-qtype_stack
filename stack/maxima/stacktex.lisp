e492b915c7e6dbde9bdaecff0d4597abfa825233
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2025-02-05 18:29:37 +0000

    Add in exception for SBCL when compiling %_ce_rem, #1385.

--- stack/maxima/stacktex.lisp
+++ stack/maxima/stacktex.lisp
@@ -495,7 +495,9 @@
 ;; Added 20 Feb 2022.
 ;; Remove %_C and %_E for display purposes.  The Maxima function %_ce_rem is defined in utils.mac
 
-(defmfun $tex1 (x) (reduce #'strcat (tex ($%_ce_rem x) nil nil 'mparen 'mparen)))
+(defmfun $tex1 (x) 
+  #+sbcl (declare (notinline $%_ce_rem))
+  (reduce #'strcat (tex ($%_ce_rem x) nil nil 'mparen 'mparen)))
 
 ;; *************************************************************************************************
 ;; Added 30 May 2022.

bf403e42305fbf0e640bfcdf1f8c571bf44fde92
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2025-06-11 16:30:14 +0100

    Update test case for PHP 7.4, JSON rounding.

--- tests/input_json_test.php
+++ tests/input_json_test.php
@@ -57,13 +57,13 @@ final class input_json_test extends qtype_stack_testcase {
         $options = new stack_options();
         $el = stack_input_factory::make('json', 'sans1', '"{}"');
         $el->set_parameter('sameType', true);
-        $state = $el->validate_student_response(['sans1' => '{"x":37.999, "y":30, "type":"Sphere", "err":null}'],
+        $state = $el->validate_student_response(['sans1' => '{"x":37.1, "y":30, "type":"Sphere", "err":null}'],
             $options, '"{}"',
                 new stack_cas_security());
         $this->assertEquals(stack_input::VALID, $state->status);
-        $this->assertEquals('"{\\"x\\":37.999, \\"y\\":30, \\"type\\":\\"Sphere\\", \\"err\\":null}"',
+        $this->assertEquals('"{\\"x\\":37.1, \\"y\\":30, \\"type\\":\\"Sphere\\", \\"err\\":null}"',
             $state->contentsmodified);
-        $this->assertEquals("<pre>{\n    \"x\": 37.999,\n    \"y\": 30,\n    " .
+        $this->assertEquals("<pre>{\n    \"x\": 37.1,\n    \"y\": 30,\n    " .
             "\"type\": \"Sphere\",\n    \"err\": null\n}</pre>",
             $state->contentsdisplayed);
     }

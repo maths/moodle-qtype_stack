6460915927fe75c18a847419f86cc6daba492964
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2025-07-31 12:53:45 +0100

    RC for Moodle 4.10.0

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025042500;
+$plugin->version   = 2025073100;
 $plugin->requires  = 2022041900;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_stack';
-$plugin->maturity  = MATURITY_ALPHA;
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '4.10.0 for Moodle 4.0+';
 
 $plugin->dependencies = [

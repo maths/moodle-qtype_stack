d57ca40ca0b515dc48206ba4fbf8b546b9b0a777
sangwinc <C.J.Sangwin@ed.ac.uk>
2026-06-29 17:08:06 +0100

    Add in filesize check to the stacklibrary checks, and rename some questions in the library.

--- cli/stacklibrarycheck.php
+++ cli/stacklibrarycheck.php
@@ -34,6 +34,8 @@ require_once($CFG->libdir . '/clilib.php');
 const MAX_FILENAME_LENGTH = 120;
 /** @var int the Maximum allowable length of a directory name. */
 const MAX_DIRNAME_LENGTH = 100;
+/** @var int the Maximum allowable size of a file, in Mb. */
+const MAX_FILE_SIZE = 2.0;
 
 /**
  * This function cretes a report starting in a particular directory.
@@ -55,6 +57,17 @@ function report($d) {
             while (($f = readdir($dh)) !== false) {
                 $fpath = "$d/$f";
 
+                if (substr($fpath, -10) == '(copy).xml') {
+                    $a[] = [$fpath, 'C', 'Filename containts "copy".'];
+                }
+
+                // Check for maximum file size.
+                $fs = filesize($fpath);
+                if (substr($fpath, -4) != '.mbz' && $fs > MAX_FILE_SIZE * 1024 * 1024) {
+                    $fs = round($fs / (1024.0*1024.0), 1);
+                    $a[] = [$fpath, 'S', 'File size of ' . $fs .  'Mb exceeds limit of ' . MAX_FILE_SIZE . ' Mb'];
+                }
+    
                 // Check for maximum filename length.
                 if (strlen($f) > MAX_FILENAME_LENGTH) {
                     $a[] = [$fpath, 'L', 'Filename exceeds limit of ' . MAX_FILENAME_LENGTH . ' characters.'];

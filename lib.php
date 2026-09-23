c842e7705eaa7110593d1527ac3f9d71962487ac
Edmund Farrow <edmund.farrow@ed.ac.uk>
2026-09-23 15:08:54 +0100

    iss1875 - Only show status page checks if STACK enabled

--- lib.php
+++ lib.php
@@ -99,5 +99,9 @@ function qtype_stack_status_checks(): array {
     global $CFG;
     require_once($CFG->dirroot . '/question/type/stack/classes/check/casconnection_check.php');
     require_once($CFG->dirroot . '/question/type/stack/classes/check/stack_version_check.php');
-    return [new \qtype_stack\check\casconnection_check(), new \qtype_stack\check\stack_version_check()];
+    if (question_bank::qtype_enabled('stack')) {
+        return [new \qtype_stack\check\casconnection_check(), new \qtype_stack\check\stack_version_check()];
+    } else {
+        return [];
+    };
 }
